00bff64735a1130c057c803bc1b70afed5d74fec
Gaël Moigne <gmoigne@live.fr>
2022-10-24 10:22:22 +0200

    ok async

--- tripsGenerator.js
+++ tripsGenerator.js
@@ -2,10 +2,11 @@ const moment = require("moment");
 const momentRandom = require("moment-random");
 const fs = require("fs");
 const fetch = require("node-fetch");
+const { response } = require("./app");
 
 const API_KEY = process.env.MAPS_API_KEY;
 // générer markup entre 1 et 3
-const NB_TRIPS = 10;
+const NB_TRIPS = 100;
 const COORDINATES_MIN = { lat: 48.5, lon: 2 };
 const COORDINATES_MAX = { lat: 49.5, lon: 3 };
 const MARKUP_MIN = 1;
@@ -51,48 +52,58 @@ function randomNumber(min, max) {
 //   }
 // }
 
-const trips = [];
+const myFunc = async function () {
+  const arr = [];
+  for (let i = 0; i < NB_TRIPS; i++) {
+    const lat = randomNumber(COORDINATES_MIN.lat, COORDINATES_MAX.lat);
+    const lon = randomNumber(COORDINATES_MIN.lon, COORDINATES_MAX.lon);
+    const lat2 = randomNumber(COORDINATES_MIN.lat, COORDINATES_MAX.lat);
+    const lon2 = randomNumber(COORDINATES_MIN.lon, COORDINATES_MAX.lon);
+    const date = momentRandom(moment().add(3, "days"), moment());
 
-for (let i = 0; i < NB_TRIPS; i++) {
-  const lat = randomNumber(COORDINATES_MIN.lat, COORDINATES_MAX.lat);
-  const lon = randomNumber(COORDINATES_MIN.lon, COORDINATES_MAX.lon);
-  const lat2 = randomNumber(COORDINATES_MIN.lat, COORDINATES_MAX.lat);
-  const lon2 = randomNumber(COORDINATES_MIN.lon, COORDINATES_MAX.lon);
-  const date = momentRandom(moment().add(3, "days"), moment());
-
-  fetch(
-    `https://dev.virtualearth.net/REST/v1/Routes/DistanceMatrix?origins=${lat},${lon}&destinations=${lat2},${lon2}&travelMode=driving&key=${API_KEY}`
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data.resourceSets[0].resources[0].results[0].travelDistance);
-      trips.push({
-        status: "Pending",
-        coordinates: {
-          lat: lat,
-          lon: lon,
-        },
-        price: randomNumber(MIN_PRICE, MAX_PRICE).toFixed(0) * 1,
-        pickupCoordinates: {
-          lat: lat2,
-          lon: lon2,
-        },
-        clientNote:
-          randomNumber(CLIENT_NOTE_MIN, CLIENT_NOTE_MAX).toFixed(2) * 1,
-        markup: randomNumber(MARKUP_MIN, MARKUP_MAX).toFixed(2) * 1,
-        date: date.toDate(),
-        distance: data.resourceSets[0].results[0].resources[0].travelDistance,
-      });
-    });
-}
-
-// const sortedTrips = trips.sort((a, b) => a.date - b.date);
-fs.writeFile("./trips.json", JSON.stringify(trips), (err) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(
-      `${NB_TRIPS} trips have been generated in trips.json file. Happy hackathon!`
+    const mapsFetch = await fetch(
+      `https://dev.virtualearth.net/REST/v1/Routes/DistanceMatrix?origins=${lat},${lon}&destinations=${lat2},${lon2}&travelMode=driving&key=${API_KEY}`
     );
+    const response = await mapsFetch.json();
+    const distance =
+      response.resourceSets[0].resources[0].results[0].travelDistance.toFixed(
+        1
+      ) * 1;
+    const travelTime =
+      response.resourceSets[0].resources[0].results[0].travelDuration.toFixed(
+        1
+      ) * 1;
+
+    arr.push({
+      status: "Pending",
+      coordinates: {
+        lat: lat,
+        lon: lon,
+      },
+      price: randomNumber(MIN_PRICE, MAX_PRICE).toFixed(0) * 1,
+      pickupCoordinates: {
+        lat: lat2,
+        lon: lon2,
+      },
+      clientNote: randomNumber(CLIENT_NOTE_MIN, CLIENT_NOTE_MAX).toFixed(2) * 1,
+      markup: randomNumber(MARKUP_MIN, MARKUP_MAX).toFixed(2) * 1,
+      date: date.toDate(),
+      distance: distance,
+      travelTime: travelTime,
+    });
   }
+  return arr;
+};
+
+myFunc().then((res) => {
+  const sortedTrips = res.sort((a, b) => a.date - b.date);
+  fs.writeFile("./trips.json", JSON.stringify(sortedTrips), (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(
+        `${NB_TRIPS} trips have been generated in trips.json file. Happy hackathon!`
+      );
+    }
+  });
 });
