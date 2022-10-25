13937756a847b4726e2ec2926d5aabb8e5357fa9
Gaël Moigne <gmoigne@live.fr>
2022-10-25 13:12:43 +0200

    node fetch à autojs

--- auto.js
+++ auto.js
@@ -1,10 +1,18 @@
-const refreshServer = async function () {
-await fetch('https://backend-providers-wine.vercel.app/uber/server-delete')
-await fetch('https://backend-providers-wine.vercel.app/heetch/server-delete')
-await fetch('https://backend-providers-wine.vercel.app/bolt/server-delete')
-await fetch('https://backend-providers-wine.vercel.app/uber/server-generate/200')
-await fetch('https://backend-providers-wine.vercel.app/heetch/server-generate/200')
-await fetch('https://backend-providers-wine.vercel.app/bolt/server-generate/200')
-}
+const fetch = require("node-fetch");
 
-refreshServer()
+const refreshServer = async function () {
+  await fetch("https://backend-providers-wine.vercel.app/uber/server-delete");
+  await fetch("https://backend-providers-wine.vercel.app/heetch/server-delete");
+  await fetch("https://backend-providers-wine.vercel.app/bolt/server-delete");
+  await fetch(
+    "https://backend-providers-wine.vercel.app/uber/server-generate/200"
+  );
+  await fetch(
+    "https://backend-providers-wine.vercel.app/heetch/server-generate/200"
+  );
+  await fetch(
+    "https://backend-providers-wine.vercel.app/bolt/server-generate/200"
+  );
+};
+
+refreshServer();
