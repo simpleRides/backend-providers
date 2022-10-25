cef295e96a6166b0e9932f8716615ca0405d0a1f
Gaël Moigne <gmoigne@live.fr>
2022-10-25 13:33:57 +0200

    fix auto script

--- auto.js
+++ auto.js
@@ -1,18 +1,36 @@
 const fetch = require("node-fetch");
 
 const refreshServer = async function () {
-  await fetch("https://backend-providers-wine.vercel.app/uber/server-delete");
-  await fetch("https://backend-providers-wine.vercel.app/heetch/server-delete");
-  await fetch("https://backend-providers-wine.vercel.app/bolt/server-delete");
+  await fetch("https://backend-providers-wine.vercel.app/uber/server-delete", {
+    method: "DELETE",
+  }).then(() => console.log("Uber Suppression Success"));
   await fetch(
-    "https://backend-providers-wine.vercel.app/uber/server-generate/200"
-  );
+    "https://backend-providers-wine.vercel.app/heetch/server-delete",
+    {
+      method: "DELETE",
+    }
+  ).then(() => console.log("Heetch Suppression Success"));
+  await fetch("https://backend-providers-wine.vercel.app/bolt/server-delete", {
+    method: "DELETE",
+  }).then(() => console.log("Bolt SuppressionSuccess"));
   await fetch(
-    "https://backend-providers-wine.vercel.app/heetch/server-generate/200"
-  );
+    "https://backend-providers-wine.vercel.app/uber/server-generate/5",
+    {
+      method: "POST",
+    }
+  ).then(() => console.log("Uber Generation Success"));
   await fetch(
-    "https://backend-providers-wine.vercel.app/bolt/server-generate/200"
-  );
+    "https://backend-providers-wine.vercel.app/heetch/server-generate/5",
+    {
+      method: "POST",
+    }
+  ).then(() => console.log("Heetch Generation Success"));
+  await fetch(
+    "https://backend-providers-wine.vercel.app/bolt/server-generate/5",
+    {
+      method: "POST",
+    }
+  ).then(() => console.log("Bolt Generation Success"));
 };
 
 refreshServer();
