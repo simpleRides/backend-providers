36c49d06e723d44af512f94dbf43b68ba5784341
Gaël <99651838+GaelMGN@users.noreply.github.com>
2022-10-31 11:53:37 +0100

    Update auto.js

--- auto.js
+++ auto.js
@@ -3,16 +3,16 @@ const fetch = require("node-fetch");
 const refreshServer = async function () {
   await fetch("https://backend-providers-wine.vercel.app/uber/server-refresh", {
     method: "PUT",
-  }).then(() => console.log("Uber Suppression Success"));
+  }).then(() => console.log("Uber Refresh Success"));
   await fetch(
     "https://backend-providers-wine.vercel.app/heetch/server-refresh",
     {
       method: "PUT",
     }
-  ).then(() => console.log("Heetch Suppression Success"));
+  ).then(() => console.log("Heetch Refresh Success"));
   await fetch("https://backend-providers-wine.vercel.app/bolt/server-refresh", {
     method: "PUT",
-  }).then(() => console.log("Bolt SuppressionSuccess"));
+  }).then(() => console.log("Bolt Refresh Success"));
 };
 
 refreshServer();
